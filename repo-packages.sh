81c6ce3e211a1c842ee0fc8738fa993cf19550ca
Lenus Walker <walker.lenus@gmail.com>
2024-09-02 22:54:49 -0400

    attempt to use chezmoi

--- repo-packages.sh
+++ repo-packages.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
-home_dir="$(getent passwd $(id -u) | awk '{print $6}' FS=':')"
-bashrc_dir="/run/host/${home_dir}/distrobox/home/devbox"
+# home_dir="$(getent passwd $(id -u) | awk '{print $6}' FS=':')"
+# bashrc_dir="/run/host/${home_dir}/distrobox/home/devbox"
 
-if ! [ -d "${bashrc_dir}" ]; then
-    mkdir -p "${bashrc_dir}"
-fi
+# if ! [ -d "${bashrc_dir}" ]; then
+#     mkdir -p "${bashrc_dir}"
+# fi
 
 # Install fastfetch
 add-apt-repository ppa:zhangsongcui3371/fastfetch
@@ -18,11 +18,11 @@ curl -LOo /opt/nvim-linux64.tar.gz https://github.com/neovim/neovim/releases/lat
 # tar -C /opt -xzf nvim-linux64.tar.gz
 tar -C /opt -xzf /opt/nvim-linux64.tar.gz
 rm /opt/nvim-linux64.tar.gz
-cat >> ${bashrc_dir}/.bashrc << EOF
+# cat >> ${bashrc_dir}/.bashrc << EOF
 
-# neovim
-export PATH="$PATH:/opt/nvim-linux64/bin"
-EOF
+# # neovim
+# export PATH="$PATH:/opt/nvim-linux64/bin"
+# EOF
 
 # Install vscode
 wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
@@ -47,9 +47,12 @@ apt install -y code
 
 # Install Starship
 curl -sS https://starship.rs/install.sh | sh -s -- --yes
-cat >> ${bashrc_dir}/.bashrc << EOF
+# cat >> ${bashrc_dir}/.bashrc << EOF
 
-# Starship
-eval "$(starship init bash)"
-fastfetch
-EOF
+# # Starship
+# eval "$(starship init bash)"
+# fastfetch
+# EOF
+
+# Install chezmoi
+sh -c "$(curl -fsLS get.chezmoi.io)" -- init --apply git@github.com:lenuswalker/dotfiles.git
