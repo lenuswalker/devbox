03e8e73059c1d4a1e5e6ad6e677e6bb5d0275c02
Lenus Walker <walker.lenus@gmail.com>
2024-09-04 21:55:08 -0400

    Update dotfile setup script

--- dotfile-setup.sh
+++ dotfile-setup.sh
@@ -1,9 +1,5 @@
 #!/usr/bin/env bash
 
-# Bitwarden login
-echo "Logging into Bitwarden..."
-bw login
-
 # Install chezmoi and apply dotfiles
 echo "Installing chezmoi and setting up dotfiles..."
 sh -c "$(curl -fsLS get.chezmoi.io)" -- init --apply lenuswalker
